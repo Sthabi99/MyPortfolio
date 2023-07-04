028797df9bc25bb4238f31978395624583df0d5d
Sthabi99 <137986434+Sthabi99@users.noreply.github.com>
2023-07-04 14:26:49 +0200

    Add files via upload

---
+++ Chapter3/chapter3.js
@@ -0,0 +1,33 @@
+let book;
+book = "JavaScript For Kids For Dummies";
+console.log(book);
+book = "The Call of the Wild";
+console.log(book);
+"I am a string.".indexOf("am");
+"I am a string.".indexOf("I");
+// < 10 // Is 1 less than 10?
+prompt("What is your name?");
+let username = prompt("What is your name?");
+alert("Good job!");
+let myNameIs = "your name";
+alert(myNameIs);
+let firstName = "your name";
+let yourScore = 30;
+alert("Hi, " + firstName + ". Your current score is: " + yourScore);
+
+// truck.color="yellow";
+// truck.drive();
+
+document.write("Hi, Mom!");
+document.write(333 + 100);
+document.write("How are you?<br>");
+document.write("I'm great! Thanks!<br>");
+
+let toName = "your name";
+let fromName = "The Grammy Awards";
+let letterBody =
+  "We are pleased to inform you that your song,'Can't Stop Coding!,' has been voted the Best Songof All Time by the awarding committee.";
+document.write("Dear " + toName + ",<br><br>");
+document.write(letterBody + "<br><br>");
+document.write("Sincerely,<br>");
+document.write(fromName);
